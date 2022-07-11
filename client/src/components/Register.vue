b5689a62581dce7037602b2ac53d04e133487b59
Sunny <sunny@Sunnyde-MacBook-Pro.local>
2022-07-11 17:03:22 +0800

    register page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input
+      type="text"
+      name="account"
+      v-model="account"
+      placeholder="account">
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password">
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthencationService from '@/services/AuthenticationService'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      account:'',
+      password:''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthencationService.register({
+        account: this.account,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
